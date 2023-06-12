76f30375f2a6ea10839bff769ac20ff826405bbc
Maulana Yusuf Azmi <ucupio@gmail.com>
2023-06-12 18:19:56 +0700

    add tailwind config

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
+import Login from './login';
+import './index.css';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="nutech-react" />
+      <Login />
     </div>
   );
 }
